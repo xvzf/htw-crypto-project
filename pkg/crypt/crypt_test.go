fb1a0f00675745cec03b8bcde4a5a695558b394a
Matthias Riegler <matthias.riegler@taotesting.com>
2020-11-06 14:05:33 +0100

    Added Benchmarks
    Results on MBP I7-9750H:
    
    Automated Multicore Benchmark: 1.59GB/s
    Single Core: 250 MB/s
    
    Signed-off-by: Matthias Riegler <matthias.riegler@taotesting.com>

--- pkg/crypt/crypt_test.go
+++ pkg/crypt/crypt_test.go
@@ -1,11 +1,44 @@
 package crypt
 
 import (
+	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/xvzf/htw-crypto-project/pkg/image"
 )
+
+var (
+	cipher            *Container
+	testData1MByte    string
+	testData1MByteEnc []PixelPosition
+)
+
+func init() {
+
+	var err error
+
+	i := image.Mock()
+	for !image.CheckAccept(i) {
+		i = image.Mock()
+	}
+
+	// Build new crypt engine
+	cipher, err = New(i)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// initialize testdata
+	// We want 1 MByte testdata for benchmarking
+	buf := make([]byte, 1024*1024)
+	for i, _ := range buf {
+		buf[i] = byte(i % 128)
+	}
+
+	testData1MByte = string(buf)
+	testData1MByteEnc, _ = cipher.Encrypt(testData1MByte)
+}
 
 func Test_ExtractGroups(t *testing.T) {
 
@@ -38,23 +71,12 @@ func TestContainer_Encrypt_Decrypt(t *testing.T) {
 	// Build new crypt engine
 	cipher, err := New(i)
 	assert.NoError(t, err)
-
-	genAllASCII := func() string {
-		buf := make([]byte, 128)
-
-		for i := 0; i < 128; i++ {
-			buf[i] = byte(i)
-		}
-
-		return string(buf)
-	}
-
 	for _, s := range []string{
 		"hello",
 		"world",
 		"abcdefhijklmnopqrstuvwxyz",
 		"this123isatestencryotun12523sa[@$!@{K352!21]5'|,125}]",
-		genAllASCII(),
+		testData1MByte,
 	} {
 		// Encrypt
 		enc, err := cipher.Encrypt(s)
@@ -66,4 +88,32 @@ func TestContainer_Encrypt_Decrypt(t *testing.T) {
 		// assert decrypted is the same
 		assert.Equal(t, s, dec)
 	}
+}
+
+func BenchmarkContainer_Encrypt1MByte(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cipher.Encrypt(testData1MByte)
+		}
+	})
+}
+
+func BenchmarkContainer_Encrypt1024(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		cipher.Encrypt(testData1MByte)
+	}
+}
+
+func BenchmarkContainer_Decrypt1024(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		cipher.Decrypt(testData1MByteEnc)
+	}
+}
+
+func BenchmarkContainer_Decrypt1MByte(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			cipher.Decrypt(testData1MByteEnc)
+		}
+	})
 }
